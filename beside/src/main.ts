d38dc2332a31a7bbbff2609b8ee0f5da0d0764e8
Nur Chamdani <92252078+nurChamdanis@users.noreply.github.com>
2024-08-10 06:49:17 +0700

    upload  all file
    upload  all file

--- beside/src/main.ts
+++ beside/src/main.ts
@@ -1,24 +1,39 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+// import helmet from 'helmet';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   app.use(helmet());
+//   app.enableCors();
+//   app.setGlobalPrefix('api');
+
+//   const config = new DocumentBuilder()
+//     .setTitle('prime nestjs')
+//     .setDescription('Boilerplate for nestjs')
+//     .setVersion('1.0')
+//     .addTag('api')
+//     .build();
+
+//   const document = SwaggerModule.createDocument(app, config);
+//   SwaggerModule.setup('api', app, document);
+
+//   await app.listen(3031);
+// }
+// bootstrap();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import helmet from 'helmet';
+import { VercelRequest, VercelResponse } from '@vercel/node'; // Import the correct types
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(helmet());
-  app.enableCors();
-  app.setGlobalPrefix('api');
-
-  const config = new DocumentBuilder()
-    .setTitle('prime nestjs')
-    .setDescription('Boilerplate for nestjs')
-    .setVersion('1.0')
-    .addTag('api')
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-
-  await app.listen(3031);
+  app.enableCors(); // Enable CORS if necessary
+  return app.init();
 }
-bootstrap();
+
+export default async (req: VercelRequest, res: VercelResponse) => {
+  const app = await bootstrap();
+  await app.getHttpAdapter().getInstance().handle(req, res);
+};
